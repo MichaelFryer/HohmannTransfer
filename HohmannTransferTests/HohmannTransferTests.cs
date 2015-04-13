954cdf6526050e3e10ac04e8a8667207b05f950d
Zwa333 <zwa@zwa.id.au>
2015-04-13 20:36:51 +1000

    Added license header to new Tests file

--- HohmannTransferTests/HohmannTransferTests.cs
+++ HohmannTransferTests/HohmannTransferTests.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿//HohmannTransfer Calculator
+//Copyright (C) 2015 Michael Fryer
+//
+//HohmannTransfer Calculator is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//HohmannTransfer Calculator is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with HohmannTransfer Calculator.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Hohmann_Transfer;
 
